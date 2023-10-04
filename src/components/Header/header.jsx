67ebd86bf13dc963f429f3674688959122d378c8
saifurrhaman <saifurrhaman2002@gmail.com>
2023-10-05 00:27:05 +0600

    new line addeded in header section addeded

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -4,7 +4,7 @@ import logo from '../../../src/assets/logo.png'
 export default function Header() {
   return (
     <div>
-      <h1>this is Header</h1>
+      <h1>this is Header section</h1>
        <div className=' flex flex-col items-center'>
         <img src={logo} alt="" />
         <h2>Journalism Without Fear or Favour</h2>
